7c5e934c22b0519ef437913faf4baf098e7755fc
Pawel Zawada <zawadapawelmail@gmail.com>
2021-07-30 20:48:02 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask.views import MethodView
+from flask_smorest import Api, Blueprint
+
+from src.geometry import LocationSchema, Location
+
+
+class Config:
+    API_TITLE = 'Activity Recognition Engine API'
+    API_VERSION = 'v0.1'
+    OPENAPI_VERSION = '3.0.2'
+    OPENAPI_URL_PREFIX = "/docs"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger"
+    OPENAPI_SWAGGER_UI_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.24.2/"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+api = Api(app)
+
+blp = Blueprint(
+    'locations', 'locations',
+    description='Operations on locations'
+)
+
+
+@blp.route('/locations')
+class Locations(MethodView):
+
+    @blp.arguments(LocationSchema)
+    @blp.response(201, LocationSchema)
+    def post(self, new_data):
+        """Ping a new location"""
+        item = Location(**new_data)
+        return item
+
+
+api.register_blueprint(blp)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
---
+++ src/geometry.py
@@ -0,0 +1,134 @@
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+from typing import List
+
+### Geometry
+from marshmallow.validate import OneOf, Equal
+
+
+class Geometry:
+    type: str
+    coordinates: List[float]
+
+    def __init__(self, type: str, coordinates: List[float]) -> None:
+        self.type = type
+        self.coordinates = coordinates
+
+
+class GeometrySchema(Schema):
+    type = fields.Str(validate=Equal('Point'), default='Point')
+    coordinates = fields.List(fields.Float())
+
+
+### App details
+
+class AppDetails:
+    version: str
+    environment: str
+
+    def __init__(self, version: str, environment: str) -> None:
+        self.version = version
+        self.environment = environment
+
+
+class AppDetailsSchema(Schema):
+    version = fields.Str()
+    environment = fields.Str(validate=OneOf(['PRODUCTION', 'DEVELOPMENT']), default='PRODUCTION')
+
+
+### Battery
+
+class Battery:
+    is_charging: bool
+    level: float
+
+    def __init__(self, is_charging: bool, level: float) -> None:
+        self.is_charging = is_charging
+        self.level = level
+
+
+class BatterySchema(Schema):
+    is_charging = fields.Bool()
+    level = fields.Float()
+
+
+### Location properties
+
+class LocationProperties:
+    timestamp: int
+    speed: float
+    speed_accuracy: int
+    heading: float
+    heading_accuracy: int
+    coordinate_accuracy: int
+    altitude_accuracy: float
+    is_mock: bool
+    is_sample: bool
+
+    def __init__(self, timestamp: int, speed: float, speed_accuracy: int, heading: float, heading_accuracy: int,
+                 coordinate_accuracy: int, altitude_accuracy: float, is_mock: bool, is_sample: bool) -> None:
+        self.timestamp = timestamp
+        self.speed = speed
+        self.speed_accuracy = speed_accuracy
+        self.heading = heading
+        self.heading_accuracy = heading_accuracy
+        self.coordinate_accuracy = coordinate_accuracy
+        self.altitude_accuracy = altitude_accuracy
+        self.is_mock = is_mock
+        self.is_sample = is_sample
+
+
+class LocationPropertiesSchema(Schema):
+    class Meta:
+        ordered = True
+
+    timestamp = fields.Int()
+    speed = fields.Float()
+    speed_accuracy = fields.Int()
+    heading = fields.Float()
+    heading_accuracy = fields.Int()
+    coordinate_accuracy = fields.Int()
+    altitude_accuracy = fields.Float()
+    is_mock = fields.Bool()
+    is_sample = fields.Bool()
+
+
+### Properties
+
+class Properties:
+    location_properties: LocationProperties
+    battery: Battery
+    app_details: AppDetails
+
+    def __init__(self, location_properties: LocationProperties, battery: Battery, app_details: AppDetails) -> None:
+        self.location_properties = location_properties
+        self.battery = battery
+        self.app_details = app_details
+
+
+class PropertiesSchema(Schema):
+    location_properties = fields.Nested(LocationPropertiesSchema)
+    battery = fields.Nested(BatterySchema)
+    app_details = fields.Nested(AppDetailsSchema)
+
+
+### Location
+
+class Location:
+    type: str
+    geometry: Geometry
+    properties: Properties
+
+    def __init__(self, type: str, geometry: Geometry, properties: Properties) -> None:
+        self.type = type
+        self.geometry = geometry
+        self.properties = properties
+
+
+class LocationSchema(Schema):
+    class Meta:
+        ordered = True
+
+    type = fields.Str(validate=Equal('Feature'), default='Feature')
+    geometry = fields.Nested(GeometrySchema)
+    properties = fields.Nested(PropertiesSchema)
