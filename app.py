373d64e0a9ca9ead9b7867e543dd905976aa9c73
Pawel Zawada <zawadapawelmail@gmail.com>
2021-08-26 16:42:30 +0200

    verify JWT token in `Authorization` header

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sentry_sdk
 import werkzeug
 from flask import Flask, json, request
 from flask.views import MethodView
+from flask_jwt_extended import jwt_required, JWTManager, current_user
 from flask_smorest import Api, Blueprint
 from sentry_sdk.integrations.flask import FlaskIntegration
 
@@ -19,12 +20,14 @@ class Config:
     OPENAPI_URL_PREFIX = "/docs"
     OPENAPI_SWAGGER_UI_PATH = "/swagger"
     OPENAPI_SWAGGER_UI_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.24.2/"
-    JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = True
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+jwt = JWTManager(app)
 
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_URI"),
@@ -59,22 +62,31 @@ def handle_bad_request(e):
     return e
 
 
+# Register a callback function that loades a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data) -> str:
+    return jwt_data["sub"]
+
+
 @blp.route('/locations')
 class Locations(MethodView):
 
     @blp.arguments(LocationSchema)
     @blp.response(201)
+    @jwt_required(locations='headers')
     def post(self, new_data):
         """Ping a new location"""
-        token = request.headers.get('Authorization').replace("Bearer ", "")
-        decoded = jwt.decode(token, options={"verify_signature": False}, algorithms=["HS256"])
-        print(decoded)
-
         item = Location(**new_data)
 
         response = supabase.client.table('locations').insert(
-            {'timestamp': item.properties['location_properties']['timestamp'].isoformat(),
-             'geojson': json.dumps(item.serialize())}).execute()
+            {
+                'timestamp': item.properties['location_properties']['timestamp'].isoformat(),
+                'geojson': json.dumps(item.serialize()),
+                'user_id': "" + current_user
+            }).execute()
 
         print(response)
 
